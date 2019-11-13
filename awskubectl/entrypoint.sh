6ae0d4f6379511c89dbca2e50c26d6b708670f1a
incizzle <incizzle@gmail.com>
2019-11-13 11:59:12 -0500

    awskubectl

---
+++ awskubectl/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+
+# Respect AWS_DEFAULT_REGION if specified
+[ -n "$AWS_DEFAULT_REGION" ] || export AWS_DEFAULT_REGION=us-east-1
+
+# Respect AWS_DEFAULT_OUTPUT if specified
+[ -n "$AWS_DEFAULT_OUTPUT" ] || export AWS_DEFAULT_OUTPUT=json
+
+# Capture output
+sh -c "aws eks update-kubeconfig --name ${AWS_EKS_CLUSTER}"
+sh -c "kubectl $*"
